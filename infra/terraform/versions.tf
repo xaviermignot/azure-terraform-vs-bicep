6d78be5e6d8838086f9b98d500f425d08428ea67
Xavier Mignot <code@xmi.fr>
2024-09-02 23:42:14 +0200

    chore: Upgrade to tofu 1.8 and azurerm 4

--- infra/terraform/versions.tf
+++ infra/terraform/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~> 4.0"
     }
     random = {
       source  = "hashicorp/random"
@@ -10,7 +10,7 @@ terraform {
     }
   }
 
-  required_version = "~> 1.7.0"
+  required_version = "~> 1.8.0"
 
   backend "azurerm" {
     key = "state.tfstate"
