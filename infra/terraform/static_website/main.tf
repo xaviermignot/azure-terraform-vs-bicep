9297df67d89a692b72c1e558bf60bf1600dc8223
Xavier Mignot <code@xmi.fr>
2025-04-28 09:36:03 +0200

    chore: Use new tf resource for static website

--- infra/terraform/static_website/main.tf
+++ infra/terraform/static_website/main.tf
@@ -9,11 +9,12 @@ resource "azurerm_storage_account" "account" {
   public_network_access_enabled = true
   https_traffic_only_enabled    = true
   min_tls_version               = "TLS1_2"
+}
 
-  static_website {
-    index_document     = "index.html"
-    error_404_document = "error.html"
-  }
+resource "azurerm_storage_account_static_website" "static_website" {
+  storage_account_id = azurerm_storage_account.account.id
+  index_document     = "index.html"
+  error_404_document = "error.html"
 }
 
 # resource "azurerm_storage_container" "some_container" {
